a6ad2a5cc08eed1d9bc5ff9aa8fbfbabbe6d4814
azhurat <azhurat@twilio.com>
2019-03-20 12:23:11 +0200

    Backpressure in action

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,9 +1,10 @@
 import java.time.{Instant, LocalDateTime}
+import java.util.concurrent.TimeoutException
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings, Supervision}
 import akka.stream.alpakka.json.scaladsl.JsonReader
 import akka.stream.scaladsl.{Sink, Source}
 import io.circe.Json
@@ -45,30 +46,52 @@ object App {
    * 4. Filter, Group By, Map/Reduce, Count the records etc.
    */
   def main(args: Array[String]): Unit = {
+    val decider: Supervision.Decider = {
+      case _: ArithmeticException ⇒ Supervision.Resume
+      case t: TimeoutException ⇒
+        println()
+        println(t)
+        println()
+        Supervision.Resume
+      case _                      ⇒ Supervision.Stop
+    }
+
     implicit val system: ActorSystem = ActorSystem()
-    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    implicit val materializer: ActorMaterializer = ActorMaterializer(ActorMaterializerSettings(system).withSupervisionStrategy(decider))
     implicit val ec: ExecutionContext = system.dispatcher
 
     //https://doc.akka.io/docs/akka/current/stream/index.html?language=scala
     //https://doc.akka.io/docs/akka-http/current/index.html?language=scala
     //https://doc.akka.io/docs/alpakka/current/
 
-    Source.tick(0.seconds, 10.seconds, None).mapAsync(1) { _ =>
-      println(s"Time of request: ${LocalDateTime.now}")
-      Http().singleRequest(HttpRequest(uri = Uri(apiUrl)))
-    }.flatMapConcat(response => response.entity.dataBytes)
+    /**
+      * Backpressure in action.
+      *
+      * throttle is backpressuring the tick.
+      */
+    Source
+      .tick(0.seconds, 10.seconds, None)
+      .mapAsync(parallelism = 1) { _ =>
+        println(s"Time of request: ${LocalDateTime.now}")
+        Http().singleRequest(HttpRequest(uri = Uri(apiUrl))).flatMap(_.entity.toStrict(1.second).map(_.data))
+      }
       .via(
         JsonReader.select("$.acList[*]")
       )
       .map(_.utf8String)
       .map { jsonStr =>
         parse(jsonStr).getOrElse(Json.Null).as[AircraftState]
-      }.collect {
-      case Right(data) =>
-        data
-    }.filter {
-      aircraftState =>
-        aircraftState.countryOfRegistration == "United Kingdom"
-    }.throttle(10, 5.seconds).runWith(Sink.foreach(println))
+      }
+      .collect {
+        case Right(data) =>
+          data
+      }
+      .filter {
+        aircraftState =>
+          //aircraftState.countryOfRegistration == "United Kingdom"
+          aircraftState.aircraftModel.contains("737")
+      }
+      .throttle(elements = 10, per = 5.seconds)
+      .runWith(Sink.foreach(println))
   }
 }
